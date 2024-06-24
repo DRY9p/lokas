e38893c9e7472233e9ba2cb6491300b197bbc316
Cr9Co0 <alburton32@gmail.com>
2024-06-24 15:30:58 +0500

    update deploy path

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :branch, 'main'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/home/deploy/#{fetch :application}"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -29,13 +29,12 @@ set :branch, 'main'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
-set :default_env, { path: "/home/deploy/lokas/.rbenv-vars" }
-#Ex:- :default =>''
+
 # Default value for local_user is ENV['USER']
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
